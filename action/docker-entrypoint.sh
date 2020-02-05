b29a649d09c600a5bf2d9cb414490b9272e27257
mutantmonkey <mutantmonkey@mutantmonkey.in>
2020-02-04 23:47:51 -0800

    Override $HOME

--- action/docker-entrypoint.sh
+++ action/docker-entrypoint.sh
@@ -6,12 +6,12 @@ export BUILDDIR=/build
 export PKGDEST=$GITHUB_WORKSPACE/repo
 export SRCDEST=/srcdest
 export SRCPKGDEST=/srcpkgdest
+export HOME=/build
 
 # this is required to build some packages
 export SHELL=/bin/bash
 
 # create a temporary gnupg keyring
-export GNUPGHOME=/build/.gnupg
 if [[ -n "$GNUPG_PUBKEYRING" ]]; then
     gpg2 --import "$GNUPG_PUBKEYRING"
 fi

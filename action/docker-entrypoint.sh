c7c0c363ef5e8348e67d6e35033469a9b620c9a9
mutantmonkey <mutantmonkey@mutantmonkey.in>
2020-02-04 23:25:12 -0800

    Add GitHub Action

---
+++ action/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+set -e
+
+export REPONAME=geschenkerbauer
+export BUILDDIR=/build
+export PKGDEST=/repo
+export SRCDEST=/srcdest
+export SRCPKGDEST=/srcpkgdest
+
+# this is required to build some packages
+export SHELL=/bin/bash
+
+# create a temporary gnupg keyring
+export GNUPGHOME=/build/.gnupg
+if [[ -n "$GNUPG_PUBKEYRING" ]]; then
+    gpg2 --import "$GNUPG_PUBKEYRING"
+fi
+
+sudo pacman -Syu --noconfirm
+
+cd $(mktemp -d /var/tmp/buildsrc-XXXXXXXXXX)
+cp -a $GITHUB_WORKSPACE/buildsrc/. .
+
+makepkg_args="-s --noconfirm $@"
+makepkg $makepkg_args

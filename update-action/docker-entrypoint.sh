0ed7a612b963eb8b81c76381cd534a39e39526cc
mutantmonkey <mutantmonkey@mutantmonkey.in>
2020-02-21 20:10:52 -0800

    update-action: fix branch_by_package

--- update-action/docker-entrypoint.sh
+++ update-action/docker-entrypoint.sh
@@ -12,8 +12,8 @@ cd "${GITHUB_WORKSPACE}" || exit 1
 declare -A branch_by_package
 while IFS=$'\n' read -ra line; do
     read -ra entry <<< "${line/,Update/ }"
-    if [[ -n "${entry[0]}" ]] && [[ -n "${entry[2]}" ]]; then
-        branch_by_package+=([${entry[0]}]=${entry[2]})
+    if [[ -n "${entry[1]}" ]] && [[ -n "${entry[2]}" ]]; then
+        branch_by_package+=([${entry[1]}]=${entry[2]})
     fi
 done <<< "$(hub pr list -f '%I,%t %H%n')"
 

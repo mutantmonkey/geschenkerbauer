f83c85b2c755b2cc2c2447eb6cd5780ad66819c7
mutantmonkey <mutantmonkey@mutantmonkey.in>
2023-03-15 01:01:04 -0700

    unpack_and_sign.sh: ensure PKGBUILD matches

--- unpack_and_sign.sh
+++ unpack_and_sign.sh
@@ -14,6 +14,11 @@ if [ -z "$SIGNIFY_PUBLIC_KEY" ]; then
     echo "No signify public key provided, signature checks will be skipped."
 fi
 
+# Warn if no source directory is provided
+if [ -z "$SOURCE_PATH" ]; then
+    echo "No source directory provided, PKGBUILD integrity checking will be skipped."
+fi
+
 for f in *.pkg.tar.*; do
     # GitHub Actions forbids : in filenames, so the build action replaces them
     # before creating the ZIP. Now that we have the file, rename it back.
@@ -32,6 +37,28 @@ for f in *.pkg.tar.*; do
     fi
 
     if [ ! -f "$OUTPUT_REPO/$f" ]; then
+        # Verify that pkgbuild_sha256sum in package BUILDINFO matches the
+        # sha256sum of the current PKGBUILD in our local source directory
+        if [ -n "$SOURCE_PATH" ]; then
+            pkgbase=$(tar -xOf "$f" .BUILDINFO | grep '^pkgbase' | sed 's/^.* = //g')
+            expected_sha256sum=$(tar -xOf "$f" .BUILDINFO | grep '^pkgbuild_sha256sum' | sed 's/^.* = //g')
+            actual_sha256sum=$(sha256sum "${SOURCE_PATH}/${pkgbase}/PKGBUILD" | cut -f1 -d ' ')
+
+            # If the sha256sum doesn't match the first time, try adjusting pkgver to match
+            if [[ "${expected_sha256sum}" != "${actual_sha256sum}" ]]; then
+                pkgver=$(tar -xOf "$f" .BUILDINFO | grep '^pkgver' | sed 's/^.* = \([^\-]\+\).*$/\1/g')
+                actual_sha256sum=$(sed "s/^pkgver=.*$/pkgver=${pkgver}/" "${SOURCE_PATH}/${pkgbase}/PKGBUILD" | sha256sum - | cut -f1 -d ' ')
+            fi
+
+            # If the sha256sum still doesn't match, then we're really in trouble
+            if [[ "${expected_sha256sum}" != "${actual_sha256sum}" ]]; then
+                echo "$f: PKGBUILD sha256sum mismatch!"
+                echo "PKGBUILD used for build: ${expected_sha256sum}"
+                echo "PKGBUILD in source dir:  ${actual_sha256sum}"
+                break
+            fi
+        fi
+
         mv "$f" "$OUTPUT_REPO/$f"
         pushd "$OUTPUT_REPO" >/dev/null
         [ -n "$GPG_KEYID" ] && gpg2 --local-user $GPG_KEYID -b "$f"

11234abf81b64fb0d3f9866214d4827ed885ff65
mutantmonkey <hello@mutantmonkey.mx>
2024-07-04 13:36:24 -0700

    Remove sign-packages action
    Now that GitHub Actions can generate provenance attestations, we use
    those instead. This removes the need to use signify and store a signify
    private key on the GitHub side but still provides a high level of
    confidence that the package was built using GitHub Actions
    infrastructure from this repository.

--- actions/sign-packages/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-install -d -m 0700 /tmp/signify
-echo "$INPUT_SIGNIFY_SECRET_KEY" > /tmp/signify/pkg.sec
-
-cd $GITHUB_WORKSPACE/repo
-sha512sum --tag *.pkg.tar.* > SHA512
-signify -S -e -s /tmp/signify/pkg.sec -m SHA512 -x SHA512.sig <<< "$INPUT_SIGNIFY_PASSWORD"

1af0f9e072912fc86db0207a2547d3946eaf2532
mutantmonkey <hello@mutantmonkey.mx>
2024-08-02 13:03:30 -0700

    Update update_action_base_image.sh

--- update_action_base_image.sh
+++ update_action_base_image.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+set -e
+
 #image=docker.io/archlinux/archlinux:base-devel
 image=ghcr.io/archlinux/archlinux:base-devel
+
 digest=$(cosign verify $image --certificate-identity-regexp="https://gitlab\.archlinux\.org/archlinux/archlinux-docker//\.gitlab-ci\.yml@refs/tags/v[0-9]+\.0\.[0-9]+" --certificate-oidc-issuer=https://gitlab.archlinux.org | jq -r ".[0].critical.image[\"docker-manifest-digest\"]")
 sed -i "s#^FROM .*\$#FROM ${image}@${digest}#" action/Dockerfile
+
+#git add action/Dockerfile
+#git commit -m "Update Arch Linux Docker base image"

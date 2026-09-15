b4bb292ed4cca057d390ef5d3b9a7717fdb7971f
mutantmonkey <hello@mutantmonkey.mx>
2026-09-15 00:27:10 -0700

    pkgctl-upgrade: create /startdir, clean up

--- actions/pkgctl-update/docker-entrypoint.sh
+++ actions/pkgctl-update/docker-entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-export TERM="${TERM:-dumb}"
+export TERM=${TERM:-dumb}
 
+mkdir -p /startdir
 cp -a "$1/." /startdir
 chown -R builduser:users /startdir
 
 pushd /startdir
 
 sudo -u builduser pkgctl version upgrade .
-sudo -u builduser BUILDDIR=/tmp PKGDEST=/tmp SRCDEST=/tmp makepkg --printsrcinfo > .SRCINFO
+sudo -u builduser makepkg --printsrcinfo > .SRCINFO
 
 popd
 

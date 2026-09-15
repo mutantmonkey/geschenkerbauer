d32c8b9d998d75cf198d6f4aa0ebc7b9d3d1577c
mutantmonkey <hello@mutantmonkey.mx>
2026-09-15 00:09:14 -0700

    pkgctl-update: run pkgctl on a copy
    This fixes the permissions issues caused by trying to work in place.

--- actions/pkgctl-update/docker-entrypoint.sh
+++ actions/pkgctl-update/docker-entrypoint.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
-set -e
-pushd $1
-pkgctl version upgrade .
+#!/bin/bash
+set -euo pipefail
+
+cp -a "$1/." /startdir
+chown -R builduser:users /startdir
+
+pushd /startdir
+
+sudo -u builduser pkgctl version upgrade .
 sudo -u builduser BUILDDIR=/tmp PKGDEST=/tmp SRCDEST=/tmp makepkg --printsrcinfo > .SRCINFO
+
 popd
+
+cp /startdir/PKGBUILD "$1/PKGBUILD"
+cp /startdir/.SRCINFO "$1/.SRCINFO"
